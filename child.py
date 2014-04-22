c5c2adc3dd650c0392daa8c26b4480a5b63d1af4
hannu-s <hannu123@windowslive.com>
2014-04-22 20:04:25 +0300

    child updates

--- child.py
+++ child.py
@@ -4,6 +4,7 @@ import httplib2
 from bs4 import BeautifulSoup
 import re
 from list_tool import ListTool
+from inspection import Inspection
 
 class Child():
 	conn = None
@@ -19,10 +20,48 @@ class Child():
 		self.conn.send("Work succesful")
 		self.conn.close()
 
+	def readReddit(self):
+		h = httplib2.Http(".cache")
+		(resp, content) = h.request(self.targetUrl, "GET",
+		                            headers={'cache-control':'no-cache'})
+		soup = BeautifulSoup(content)
+		siteTable = soup.find("div", {"id": "siteTable"})
+		#remove clearleft divs
+		[s.extract() for s in siteTable('div',{'class':'clearleft'})]
+
+		insp = []
+		pages = []
+		urls = []
+		links = []
+
+		for div in siteTable.find_all('div', {'class':'entry unvoted'}):
+			urls.append(div.a.get('href'))
+			links.append(div.a.get_text())
+
+		for ind, url in enumerate(urls):
+			p = Page(url, links[ind])
+			p.followLink()
+			p.parseResults()
+			if not p.writePageData('pages/asd.txt'):
+				print('Error occured while writing page data!')
+				return
+			p.getScore()
+			p.releaseMemory()
+			pages.append(p)
+			insp.append(Inspection(links[ind], p.score, url, p.fileName))
+
+
+
 class Page():
 	score = 0
 	maxLen = 255
 	fileName = None
+	lMulti = 100
+	tMulti = 50
+	hMulti = 10
+	sMulti = 5
+	nMulti = 1
+
 	def __init__(self, url, link, title = [], header = [], spec = [], norm = []):
 		if url[0] == '/' and url[1] == 'r':
 			url = 'http://www.reddit.com' + url
@@ -59,7 +98,13 @@ class Page():
 	def parseResults(self):
 		lt = ListTool()
 		patt = "^[a-zA-Z0-9]*$"
+		self.link = self.link.split(' ')
 		self.title = self.title.split(' ')
+		for ind, item in enumerate(self.link):
+			if re.match(patt, item) == None or len(item) > self.maxLen:
+				self.link.pop(ind)
+		self.link = lt.removeDuplicates(self.link)
+
 		for ind, item in enumerate(self.title):
 			if re.match(patt, item) == None or len(item) > self.maxLen:
 				self.title.pop(ind)
@@ -81,6 +126,7 @@ class Page():
 		self.norm = lt.removeDuplicates(self.norm)
 
 	def writePageData(self, fileName):
+		self.fileName = fileName
 		t = ""
 		t = t.join(self.title) + '\n'
 		h = ""
@@ -89,23 +135,60 @@ class Page():
 		s = s.join(self.spec) + '\n'
 		n = ""
 		n = n.join(self.norm)
-		
-		f = open(fileName, 'w+')
-		f.write(self.url + '\n' + t + h + s + n)
-		f.close()
+		try:
+			f = open(fileName, 'w+')
+			f.write(self.url + '\n' + t + h + s + n)
+		except Exception as e:
+			print(e)
+			return False
+		try:
+			f.close()
+		except:
+			return False
+		return True
 
-	def getScores(self, keywords, avoids):
-		lMulti = 100
-		tMulti = 50
-		hMulti = 10
-		sMulti = 5
-		nMulti = 1
+	def getScore(self, keyWords, avoids):
+		lt = ListTool()
+		lWords = lt.getNonUniques(keyWords, self.link)
+		tWords = lt.getNonUniques(keyWords, self.title)
+		hWords = lt.getNonUniques(keyWords, self.header)
+		sWords = lt.getNonUniques(keyWords, self.spec)
+		nWords = lt.getNonUniques(keyWords, self.norm)
 
+		score = len(lWords) * self.lMulti
+		score += len(tWords) * self.tMulti
+		score += len(hWords) * self.hMulti
+		score += len(sWords) * self.sMulti
+		score += len(nWords) * self.nMulti
 
-p = Page('http://www.reddit.com/r/programming/', 'lolol asd')
+		lWords = lt.getNonUniques(avoids, self.link)
+		tWords = lt.getNonUniques(avoids, self.title)
+		hWords = lt.getNonUniques(avoids, self.header)
+		sWords = lt.getNonUniques(avoids, self.spec)
+		nWords = lt.getNonUniques(avoids, self.norm)
+
+		score -= len(lWords) * self.lMulti
+		score -= len(tWords) * self.tMulti
+		score -= len(hWords) * self.hMulti
+		score -= len(sWords) * self.sMulti
+		score -= len(nWords) * self.nMulti
+
+		self.score = score
+
+	def releaseMemory(self):
+		self.title = None
+		self.header = None
+		self.spec = None
+		self.norm = None
+'''
+p = Page('http://www.reddit.com/r/programming/', 'lolol asd kebab')
 p.followLink()
 p.parseResults()
 p.writePageData('pages/herpderp')
+p.getScore(['asd', 'kebab'],['derp'])
+p.releaseMemory()
+print(p.score)
+'''
 '''
 h = httplib2.Http(".cache")
 (resp, content) = h.request("http://www.reddit.com/r/programming/", "GET",
